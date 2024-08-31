c3781c7652ef9aecf5eef09793eebff86ab0cac2
Ayush Solanki <ayushsolanki1403@gmail.com>
2024-08-31 15:38:37 +0530

    User & Video Model

--- src/db/index.js
+++ src/db/index.js
@@ -3,13 +3,14 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       console.log(`MongoDB Connected: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+        console.log(`MongoDB Connected: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error ", error);
+        process.exit(1);
     }
-    catch (error) {
-        console.log("MONGODB connection error " , error)
-        process.exit(1)
-    }
-}
+};
 
-export default connectDB
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-    path : "./env"
-})
+    path: "./env",
+});
 
 connectDB()
-.then( () => {
-    app.listen(process.env.PORT || 8000 , () => {
-        console.log("Server is running on port 8000")
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log("Server is running on port 8000");
+        });
     })
-})
-.catch((error) => {
-    console.log("MongoDB Connection Failed in Index file" , error)
-})
+    .catch((error) => {
+        console.log("MongoDB Connection Failed in Index file", error);
+    });
